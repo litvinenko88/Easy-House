66e3a0314a6053a25e1563a8e624fcef00f544a6
litvinenko <litvinenko_n.v@mail.ru>
2025-09-07 07:52:58 +0300

    Фавикон

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,10 @@ export default function Document() {
         <meta name="googlebot" content="index, follow" />
         <meta name="author" content="Easy House" />
         <meta name="theme-color" content="#df682b" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="canonical" href="https://easy-house.ru/" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
