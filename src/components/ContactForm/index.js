d3f819e0b3127e2a2712b876f5405e8c99fef623
litvinenko <litvinenko_n.v@mail.ru>
2025-09-09 10:54:49 +0300

    Fix ContactForm export for build compatibility

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,3 +1,4 @@
-export { default as ContactForm } from './ContactForm';
+import ContactForm from './ContactForm';
 export { default as ContactFormWithPDF } from './ContactFormWithPDF';
 export { default as ContactFormModal } from './ContactFormModal';
+export default ContactForm;
