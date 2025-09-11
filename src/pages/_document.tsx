c3fb996bf23f1b4f2a2fdbc60fa3aa845b2e434b
litvinenko <litvinenko_n.v@mail.ru>
2025-09-11 17:09:56 +0300

    Добавлен счетчик Яндекс.Метрики

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,6 +23,26 @@ export default function Document() {
         <link rel="preload" href="/videos/glav2308.mp4" as="video" type="video/mp4" />
         <meta name="robots" content="noarchive, nosnippet" />
         <meta httpEquiv="X-Robots-Tag" content="noarchive, nosnippet" />
+        
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104007659', 'ym');
+              
+              ym(104007659, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/104007659" style={{position:'absolute', left:'-9999px'}} alt="" />
+          </div>
+        </noscript>
       </Head>
       <body>
         <Main />
