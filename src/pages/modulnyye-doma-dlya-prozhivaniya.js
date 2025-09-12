b54a69408ab6566af7e2f864e12b2d2a6ef49468
litvinenko <litvinenko_n.v@mail.ru>
2025-09-12 15:00:17 +0300

    Стр создана

---
+++ src/pages/modulnyye-doma-dlya-prozhivaniya.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout/Layout";
+import Bestsellers from "../components/Bestsellers/Bestsellers";
+
+export default function ModulnyeDomaDlyaProzhivaniya() {
+  return (
+    <Layout
+      title="Модульные дома для проживания | Easy House"
+      description="Модульные дома для круглогодичного проживания под ключ. Комфортные и энергоэффективные дома от производителя."
+      keywords="модульные дома для проживания, дома для круглогодичного проживания, модульные дома под ключ"></Layout>
+  );
+}
